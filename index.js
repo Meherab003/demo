ae8bef5d27f6f9551ebca15732bc23b89e2ef79a
Meherab Hosssain Bhuiyan <127032892+Meherab003@users.noreply.github.com>
2024-08-25 01:37:34 +0600

    connected with mongodb and front end server

--- index.js
+++ index.js
@@ -1,22 +1,58 @@
-const express = require('express')
+const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
 
 //MiddleWare
 const corsOptions = {
-    origin: ["http://localhost:5173", "http://localhost:5174"],
-    credentials: true,
-    optionsSuccessStatus: 200,
-  };
-  app.use(cors(corsOptions));
-  app.use(express.json());
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send({"server_name": "Genius Gala"})
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swvd3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const assignmentCollection = client
+      .db("genius-gala")
+      .collection("assignments");
+
+      //Save Created Assignments Data to the DB
+        app.post('/assignments', async(req, res)=>{
+            const assignmentData = req.body;
+            const result = await assignmentCollection.insertOne(assignmentData);
+            res.send(result)
+        })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send({ server_name: "Genius Gala" });
+});
 
 app.listen(port, () => {
-  console.log(`Genius Gala API is running on port ${port}`)
-})
+  console.log(`Genius Gala API is running on port ${port}`);
+});
